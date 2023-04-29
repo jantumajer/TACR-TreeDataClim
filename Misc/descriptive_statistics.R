06ecd7135e3c221141b557d1ae4e083d940819bf
jantumajer <tumajer1@email.cz>
2023-04-29 11:00:38 +0200

    Update descriptive_statistics.R

--- Misc/descriptive_statistics.R
+++ Misc/descriptive_statistics.R
@@ -1,24 +1,24 @@
+# This is R-script to calculate basic descriptive statistics for each site
+# Reguired open-source packages:
 library(dplR);library(readxl); library(base); library(pointRes)
+# Reguired input files
+SITE <- read.csv("sites.csv")
 
-SITE <- read.csv("E:/TACR/zchron_TestData2/META/data_april2_withCZU.csv")
 parametry <- data.frame(siteco = SITE$site_code, MeanRingWidth = NA, MeanAge = NA, FirstYear = NA, LastYear = NA)
 
 
 for (i in c(1:nrow(SITE))){
-  
-  if (!(SITE[i, "raw_data_file_name"] == "")){ # Podminka vlozena do te doby, nez Cada a Rydval poslou svoje rwl soubory
-  
+   
 serie1 <- read.rwl(paste("E:/TACR/zchron_TestData2/RWL/",SITE[i, "raw_data_file_name"], sep = "")) 
 
 stats <- rwl.stats(serie1)
 
-parametry[i, "MeanRingWidth"] <- mean(stats$mean)
-parametry[i, "MeanAge"] <- round(mean(stats$year), 1)
-parametry[i, "FirstYear"] <- min(stats$first)
-parametry[i, "LastYear"] <- max(stats$last)
+# Descriptive statistics per each site:    
+parametry[i, "MeanRingWidth"] <- mean(stats$mean) # Mean tree-ring width
+parametry[i, "MeanAge"] <- round(mean(stats$year), 1) # Mean age of sampled tree
+parametry[i, "FirstYear"] <- min(stats$first) # Oldest measured ring
+parametry[i, "LastYear"] <- max(stats$last) # Youngest measured ring (i.e., year of sampling, if the stand was living)
 
-
-  }
 }
 
-write.csv(parametry, "E:/TACR/Vysledky/descriptive_parameters.csv")
+write.csv(parametry, "descriptive_parameters.csv") # Saves the table with output files into csv
